93b3b850bf16205192242ab6916874ff06fde463
Sourabh Pal <nick.tdr19@gmail.com>
2018-03-25 12:53:09 +0530

    Completed the discover fragment's layout.

--- app/src/main/java/com/example/personal/shazamclone/utils/FragmentUtils.kt
+++ app/src/main/java/com/example/personal/shazamclone/utils/FragmentUtils.kt
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager
 /**
  * Created by personal on 3/21/2018.
  */
-
 object FragmentUtils {
 
     fun addIfNotExists(fm: FragmentManager, containerRes: Int, fragment: Fragment, tag: String )
