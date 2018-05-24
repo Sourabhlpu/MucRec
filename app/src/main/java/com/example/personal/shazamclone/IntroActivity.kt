ec2cb2be0b63557302f0ad43dfd5305ea552ea82
sourabhlpu <nick.tdr19@gmail.com>
2018-05-24 21:37:04 +0530

    Fixed the issue with the intro screen and the discover activity.

--- app/src/main/java/com/example/personal/shazamclone/IntroActivity.kt
+++ app/src/main/java/com/example/personal/shazamclone/IntroActivity.kt
@@ -3,7 +3,9 @@ package com.example.personal.shazamclone
 import agency.tango.materialintroscreen.MaterialIntroActivity
 import agency.tango.materialintroscreen.SlideFragmentBuilder
 import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import com.example.personal.shazamclone.discover.DiscoverActivity
 import com.example.personal.shazamclone.utils.PrefManager
 
 
@@ -16,39 +18,52 @@ class IntroActivity : MaterialIntroActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
 
+        if(!pref.isFirstTimeLaunch)
+        {
+            launchDiscoverActivity()
+        }
+        else {
 
+            addSlide(SlideFragmentBuilder()
+                    .backgroundColor(R.color.first_slide_background)
+                    .buttonsColor(R.color.first_slide_buttons)
+                    .image(R.drawable.search)
+                    .title(getString(R.string.slide_one_title))
+                    .description(getString(R.string.slide_one_description))
+                    .build())
 
-        addSlide(SlideFragmentBuilder()
-                .backgroundColor(R.color.first_slide_background)
-                .buttonsColor(R.color.first_slide_buttons)
-                .image(R.drawable.search)
-                .title(getString(R.string.slide_one_title))
-                .description(getString(R.string.slide_one_description))
-                .build())
+            addSlide(SlideFragmentBuilder()
+                    .backgroundColor(R.color.second_slide_background)
+                    .buttonsColor(R.color.second_slide_buttons)
+                    .image(R.drawable.save)
+                    .title(getString(R.string.slide_two_title))
+                    .neededPermissions(arrayOf(Manifest.permission.RECORD_AUDIO))
+                    .build())
 
-        addSlide(SlideFragmentBuilder()
-                .backgroundColor(R.color.second_slide_background)
-                .buttonsColor(R.color.second_slide_buttons)
-                .image(R.drawable.save)
-                .title(getString(R.string.slide_two_title))
-                .neededPermissions(arrayOf(Manifest.permission.RECORD_AUDIO))
-                .build())
+            addSlide(SlideFragmentBuilder()
+                    .backgroundColor(R.color.third_slide_background)
+                    .buttonsColor(R.color.third_slide_buttons)
+                    .image(R.drawable.play_video)
+                    .title(getString(R.string.slide_three_title))
+                    .description(getString(R.string.slide_three_description))
+                    .build())
 
-        addSlide(SlideFragmentBuilder()
-                .backgroundColor(R.color.third_slide_background)
-                .buttonsColor(R.color.third_slide_buttons)
-                .image(R.drawable.play_video)
-                .title(getString(R.string.slide_three_title))
-                .description(getString(R.string.slide_three_description))
-                .build())
-
-
+        }
 
     }
 
     override fun onFinish() {
         pref.isFirstTimeLaunch = false
+        launchDiscoverActivity()
+    }
+
+    private fun launchDiscoverActivity()
+    {
+        val intent = Intent(IntroActivity@this, DiscoverActivity :: class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/personal/shazamclone/discover/DiscoverActivity.kt
+++ app/src/main/java/com/example/personal/shazamclone/discover/DiscoverActivity.kt
@@ -1,34 +1,25 @@
 package com.example.personal.shazamclone.discover
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.personal.shazamclone.IntroActivity
 import com.example.personal.shazamclone.R
 import com.example.personal.shazamclone.utils.FragmentUtils
-import com.example.personal.shazamclone.utils.PrefManager
 
 class DiscoverActivity : AppCompatActivity() {
 
     private val REQUEST_PERMISSIONS_CODE = 100;
 
 
-    private val pref: PrefManager by lazy { PrefManager(this) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
 
         super.onCreate(savedInstanceState)
 
-        if (pref.isFirstTimeLaunch) {
-
-            launchOnboarding()
-        }
-        else if(!pref.isFirstTimeLaunch) {
 
 
-            setTheme(R.style.AppTheme)
 
-            setContentView(R.layout.activity_discover)
+        setContentView(R.layout.activity_discover)
 
 
             val discoverFragment: DiscoverFragment = DiscoverFragment()
@@ -37,21 +28,10 @@ class DiscoverActivity : AppCompatActivity() {
                     discoverFragment, "DiscoverFragment")
 
             DiscoverPresenter().takeView(discoverFragment)
-        }
+
 
 
     }
 
-    private fun launchOnboarding() {
-        val intent: Intent = Intent(DiscoverActivity@ this, IntroActivity::class.java)
-        startActivity(intent)
-    }
 
-    override fun onBackPressed() {
-
-        if(pref.isFirstTimeLaunch){
-            System.gc()
-            System.exit(0)
-        }
-    }
 }
